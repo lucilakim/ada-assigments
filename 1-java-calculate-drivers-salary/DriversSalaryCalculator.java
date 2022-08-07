6a1ea2c9f8936f7034b8cbac69dc944ec1e21c25
Hatchi <hatchi@Hatchis-MacBook-Pro.local>
2022-08-07 16:21:57 -0300

    added exercise statement

--- 1-java-calculate-drivers-salary/DriversSalaryCalculator.java
+++ 1-java-calculate-drivers-salary/DriversSalaryCalculator.java
@@ -1,3 +1,12 @@
+/*16.	Una compañía de transporte cuenta con cinco choferes, de los cuales se conoce: nombre, horas trabajadas cada día de la semana (seis días) y sueldo por hora. Realice un algoritmo que: 
+
+a.	Calcule el total de horas trabajadas a la semana para cada trabajador.
+b.	Calcule el sueldo semanal para cada uno de ellos.
+c.	Calcule el total que pagará la empresa.
+d.	Indique el nombre del trabajador que labora más horas el día lunes.
+e.	Imprima un reporte con todos los datos anteriores.
+ */
+
 class DriversSalaryCalculator {
     public static void main(String[] args) {
         System.out.println("======================================================== ");
